6984667e364d16f1db13db2708f917e83e0cd8f8
Austin <austin.indrapaul@gmail.com>
2023-01-27 09:05:29 +0530

    New Microservices Project Created

---
+++ microservices_departmentservice/src/main/java/app/MicroservicesDepartmentserviceApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDepartmentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDepartmentserviceApplication.class, args);
+	}
+
+}
